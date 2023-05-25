e4d2b76354e91fe7e06ff1deb8c14be460637c07
ng-james <86743297+ng-james@users.noreply.github.com>
2023-05-24 20:33:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ except URLError as e:
   streamlit.error()
   
 
-streamlit.header("The fruit load list contains:")
+streamlit.title("View Our Fruit List = Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -51,9 +51,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) # Geta all rows
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 
@@ -68,12 +69,13 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.text(back_from_function)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
+# # don't run anything past here while we troubleshoot
+# streamlit.stop()
 
-streamlit.header("The fruit load list contains:")
+# streamlit.header("The fruit load list contains:")
 
 
 # # Test
