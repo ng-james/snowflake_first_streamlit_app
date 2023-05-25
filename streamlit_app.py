a66a8a0530f242d746b3b4bfe961267764873ebf
ng-james <86743297+ng-james@users.noreply.github.com>
2023-05-24 17:05:34 -0700

    Update streamlit_app.py
    clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+# streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # flattens json structure
-streamlit.dataframe(fruityvice_normalized) # creates a dataframe of the normalized json
+streamlit.dataframe(fruityvice_normalized) # creates a dataframe of the normalized json and displays as a table
